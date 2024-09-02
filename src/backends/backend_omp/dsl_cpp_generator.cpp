e3a09487b95570da8e2c19b0bb190fee8da6cbb1
anubhavkhajuria <62196748+anubhavkhajuria@users.noreply.github.com>
2024-09-02 17:37:43 +0530

    Added GNN class

--- src/backends/backend_omp/dsl_cpp_generator.cpp
+++ src/backends/backend_omp/dsl_cpp_generator.cpp
@@ -43,7 +43,7 @@ namespace spomp
     header.pushString("#include");
     addIncludeToFile("omp.h", header, true);
     header.pushString("#include");
-    addIncludeToFile("../graph.hpp", header, false);
+    addIncludeToFile("../graph.cpp", header, false);
     header.pushString("#include");
     addIncludeToFile("../atomicUtil.h", header, false);
     header.NewLine();
@@ -2755,6 +2755,10 @@ namespace spomp
     else if (type->isGraphType())
     {
       return "graph&";
+    }
+    else if (type->isGNNType())
+    {
+      return "GNN ";
     }
     else if (type->isCollectionType())
     {
